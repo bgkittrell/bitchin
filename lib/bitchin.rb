0320126a8441f86355a3e02b5fd3115d4e776e63
Ben Kittrell <ben@garbageburrito.com>
2012-04-10 12:07:18 -0500

    Converting to gem for bundler

--- lib/bitchin.rb
+++ lib/bitchin.rb
@@ -1,34 +1,3 @@
 module Bitchin
-  def bitchin(*flags)
-    column = "bit_flag"
-    if flags.first.is_a? Hash
-      options = flags.shift 
-      column = options[:column] if options[:column]
-    end
-    current_flag = 1
-    for name in flags
-      
-      class_eval <<-METHOD
-      def #{name.to_s + '?'}
-        (#{current_flag} & self.#{column}) == #{current_flag}
-      end
-      
-      def #{name.to_s}
-        #{name.to_s + '?'}
-      end
-      
-      def #{name.to_s}=(f)
-        if f == true || f == "true"
-          self.#{column} |= #{current_flag}
-        else
-          if (#{current_flag} & self.#{column}) == #{current_flag}
-            self.#{column} ^= #{current_flag}
-          end
-        end
-      end
-      METHOD
-
-      current_flag <<= 1
-    end
-  end
+  # Your code goes here...
 end
